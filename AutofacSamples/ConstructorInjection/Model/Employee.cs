4e87dd3ee0f4bda8c462be38c6d58742cc43bcd3
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2015-06-27 01:46:21 +0530

    Added Employee Model Class

---
+++ AutofacSamples/ConstructorInjection/Model/Employee.cs
@@ -0,0 +1,11 @@
+﻿
+namespace ConstructorInjection.Model
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Designation { get; set; }
+    }
+}
