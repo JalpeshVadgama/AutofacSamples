6fd40cf9daa136c009c4e7bf9aaaea99f505f6e3
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2015-06-27 01:43:37 +0530

    Added constructor injection project to demonstrate constructor inejction

--- AutofacSamples/AutoFacBasic/Program.cs
+++ AutofacSamples/AutoFacBasic/Program.cs
@@ -1,15 +1,17 @@
 ﻿using Autofac;
 using System;
+
 namespace AutoFacBasic
 {
     class Program
     {
         static void Main(string[] args)
         {
-
+            //registering interface with class that implemented
             var builder = new ContainerBuilder();
             builder.RegisterType<PrintService>().As<IPrintService>();
 
+            //Resolving inteface with autofac
             var container = builder.Build();
             var printService = container.Resolve<IPrintService>();
             printService.PrintSomething("Hello World");
