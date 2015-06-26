5f699a9294af282476131b0ef3d0ac8b6b1857eb
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2015-06-27 02:06:14 +0530

    Added constructor injection code with Autofac- Added program.cs code Added print method in employee service.

--- AutofacSamples/ConstructorInjection/Program.cs
+++ AutofacSamples/ConstructorInjection/Program.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Autofac;
+using ConstructorInjection.Model;
+using ConstructorInjection.Repository;
+using ConstructorInjection.Service;
 
 namespace ConstructorInjection
 {
@@ -10,6 +9,24 @@ namespace ConstructorInjection
     {
         static void Main(string[] args)
         {
+            var container = BuildContainer();
+            var employeeService = container.Resolve<EmployeeService>();
+            Employee employee = new Employee
+            {
+                EmployeeId = 1,
+                FirstName = "Jalpesh",
+                LastName = "Vadgama",
+                Designation = "Technical Architect"
+            };
+            employeeService.Print(employee);
+
+        }
+        static IContainer BuildContainer()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<EmployeeRepository>().As<IEmployeeRepository>();
+            builder.RegisterType<EmployeeService>();
+            return builder.Build();
         }
     }
 }
---
+++ AutofacSamples/ConstructorInjection/Service/EmployeeService.cs
@@ -0,0 +1,20 @@
+﻿using ConstructorInjection.Model;
+using ConstructorInjection.Repository;
+
+namespace ConstructorInjection.Service
+{
+    public class EmployeeService
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public EmployeeService(IEmployeeRepository employeerRepository)
+        {
+            _employeeRepository = employeerRepository;
+        }
+
+        public void Print(Employee employee)
+        {
+            _employeeRepository.Print(employee);
+        }
+    }
+}
