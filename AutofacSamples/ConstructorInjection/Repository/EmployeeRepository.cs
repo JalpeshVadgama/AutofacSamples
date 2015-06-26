02ac7e315a68efba6e5dfc62a39e4e7e2558d171
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2015-06-27 01:51:46 +0530

    Added repository class and inteface for Employee

---
+++ AutofacSamples/ConstructorInjection/Repository/EmployeeRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConstructorInjection.Repository
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        public void Print(Model.Employee employee)
+        {
+            Console.WriteLine("Printing Employee");
+            Console.WriteLine(string.Format("Employee Id : {0}", employee.EmployeeId));
+            Console.WriteLine(string.Format("First Name : {0}", employee.FirstName));
+            Console.WriteLine(string.Format("Last Name : {0}", employee.LastName));
+            Console.WriteLine(string.Format("Designation : {0}", employee.Designation));
+
+        }
+    }
+}
---
+++ AutofacSamples/ConstructorInjection/Repository/IEmployeeRepository.cs
@@ -0,0 +1,9 @@
+﻿using ConstructorInjection.Model;
+
+namespace ConstructorInjection.Repository
+{
+    public interface IEmployeeRepository
+    {
+        void Print(Employee employee);
+    }
+}
