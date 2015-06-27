48e355cf2719c6e43860e0e42d8910305f67b68f
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2015-06-28 01:36:35 +0530

    Added Teacher module

---
+++ AutofacSamples/AutoFacModules/TeacherModule.cs
@@ -0,0 +1,13 @@
+﻿using Autofac;
+
+namespace AutoFacModules
+{
+    public class TeacherModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<TeacherRepository>().As<ITeacherRepository>();
+            base.Load(builder);
+        }
+    }
+}
