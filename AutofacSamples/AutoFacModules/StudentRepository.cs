2ca6b3e441af5cf43abfa17b23acc1147d3e50f0
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2015-06-28 01:52:49 +0530

    Corrected typo

--- AutofacSamples/AutoFacModules/StudentRepository.cs
+++ AutofacSamples/AutoFacModules/StudentRepository.cs
@@ -8,10 +8,10 @@ namespace AutoFacModules
         public void Print(Student student)
         {
             Console.Write("Printing Student");
-            Console.Write(string.Format("Student Id : {0) ", student.StudentId));
-            Console.Write(string.Format("First name: {0) ", student.FirstName));
-            Console.Write(string.Format("Last Name : {0) ", student.LastName));
-            Console.Write(string.Format("Department : {0) ", student.Department));
+            Console.Write(string.Format("Student Id : {0}", student.StudentId));
+            Console.Write(string.Format("First name: {0} ", student.FirstName));
+            Console.Write(string.Format("Last Name : {0} ", student.LastName));
+            Console.Write(string.Format("Department : {0} ", student.Department));
         }
     }
 }
