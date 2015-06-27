8b95eac576ebcf859e9a32222b7a68d37a98fb42
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2015-06-28 01:38:10 +0530

    Added Student Module.

---
+++ AutofacSamples/AutoFacModules/StudentModule.cs
@@ -0,0 +1,13 @@
+﻿using Autofac;
+
+namespace AutoFacModules
+{
+    public class StudentModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<StudentRepository>().As<IStudentRepository>();
+            base.Load(builder);
+        }
+    }
+}
