1cb2811f637bcb49d0df36428d267d07d8b6b82a
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2015-06-28 01:28:06 +0530

    Added teacher class

---
+++ AutofacSamples/AutoFacModules/Teacher.cs
@@ -0,0 +1,11 @@
+﻿
+namespace AutoFacModules
+{
+    public class Teacher
+    {
+        public int TeacherId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Subject { get; set; }
+    }
+}
