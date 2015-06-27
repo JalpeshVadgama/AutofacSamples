70818bdc1d70a8ea78636188e540bbf77d74555f
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2015-06-28 01:48:36 +0530

    Added register modules code and print teacher and print student code.

--- AutofacSamples/AutoFacModules/Program.cs
+++ AutofacSamples/AutoFacModules/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Autofac;
 
 namespace AutoFacModules
 {
@@ -10,7 +6,50 @@ namespace AutoFacModules
     {
         static void Main(string[] args)
         {
+            //Creating a Student object
+            Student student = new Student
+            {
+                StudentId = 1,
+                FirstName = "Vishal",
+                LastName = "Vadgama",
+                Department = "Information Technology"
+            };
+
+            //Creating a teacher object
+            Teacher teacher = new Teacher
+            {
+                TeacherId = 1,
+                FirstName = "Jalpesh",
+                LastName = "Vadgama",
+                Subject = "Computer Applciations"
+            };
+
+            //getting container object
+            var container = BuildContainer();
+
+            //Resolving student repository
+            var studentRepository = container.Resolve<IStudentRepository>();
+            //Printing student
+            studentRepository.Print(student);
+
+            //Resolving teacher repository
+            var teacherRepository = container.Resolve<ITeacherRepository>();
+            //Printing Teacher
+            teacherRepository.Print(teacher);
+
         }
+
+        static IContainer BuildContainer()
+        {
+            var builder = new ContainerBuilder();
+
+            //Registering Modules
+            builder.RegisterModule<StudentModule>();
+            builder.RegisterModule<TeacherModule>();
+
+            return builder.Build();
+        }
+
     }
 
 
