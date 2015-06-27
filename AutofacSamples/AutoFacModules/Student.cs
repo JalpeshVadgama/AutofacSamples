be21f139ed7f76f3e9e9304bfe9c681b146764b4
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2015-06-28 01:21:25 +0530

    Added a Autofac module project and student class in it

---
+++ AutofacSamples/AutoFacModules/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoFacModules
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Department { get; set; }
+    }
+}
