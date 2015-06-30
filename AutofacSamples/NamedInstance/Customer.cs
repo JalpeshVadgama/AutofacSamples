faa101c2443109c094485883e78a3eeb272ce2c6
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2015-06-30 10:29:32 +0530

    Added Customer class

---
+++ AutofacSamples/NamedInstance/Customer.cs
@@ -0,0 +1,11 @@
+﻿
+namespace NamedInstance
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+    }
+}
