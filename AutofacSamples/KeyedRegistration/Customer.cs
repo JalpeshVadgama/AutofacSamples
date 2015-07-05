d9778dbc4af2808e2d5fd53ce91aa9a50af2b5ca
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2015-07-05 15:34:37 +0530

    Added Customer class to keyed registration

---
+++ AutofacSamples/KeyedRegistration/Customer.cs
@@ -0,0 +1,10 @@
+namespace KeyedRegistration
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+    }
+}
